9cc0a6db898794bf31a8994b40ea507760b96910
Julio Nabor <julionabor@gmail.com>
2023-09-04 16:04:54 +0100

    fix bug fetch api

--- backend/app.js
+++ backend/app.js
@@ -8,11 +8,11 @@ const port = process.env.PORT;
 
 const app = express();
 // Solve CORS
-app.use(cors({ credentials: true, origin: "https://lendasdoroblox.onrender.com/" }));
+app.use(cors({ credentials: true, origin: "https://lendasdoroblox.onrender.com" }));
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	
+	res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');	
 	next();
 });
 // Config JSON and form data response
